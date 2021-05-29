e249d64587513d464cd70016fd6a0e20d16f8ef3
Dhananjay Vats <dhananjayv70031@psis266.org>
2021-05-28 21:01:07 -0400

    added night background and score

--- Pig.js
+++ Pig.js
@@ -19,7 +19,13 @@ class Pig extends BaseClass {
      pop();
    }
    
+   
  }
+ score(){
+  if(this.Visiblity<0 && this.Visiblity>-1005){
+    score++;
+  }
+}
 
 
 
--- sketch.js
+++ sketch.js
@@ -9,9 +9,11 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var bg = "sprites/bg.png"
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
 }
 
 function setup(){
@@ -45,18 +47,29 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    else{
+        background(0);
+    }
+    textSize(35);
+    fill("white");
+    text("Score: " +score,width-300,50);
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +98,18 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function getBackgroundImage(){
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Etc/GMT-8")
+    var responseJSON = await response.json();
+    var dateTime = responseJSON.datetime;
+    var hour = dateTime.slice(11,13);
+    if (hour>=00 &&bg<17){
+        bg="sprites/bg.png";
+    }
+    else{
+        bg="sprites/bg2.jpg";
+    }
+    backgroundImg=loadImage(bg);
+
 }
